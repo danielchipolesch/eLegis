c01763c8340742874cea0430c53351955fecfc36
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2023-07-30 22:21:35 -0300

    Entity Documento has been updated

--- src/main/java/br/com/elegis/api/model/entity/Documento.java
+++ src/main/java/br/com/elegis/api/model/entity/Documento.java
@@ -1,52 +1,30 @@
 package br.com.elegis.api.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name="t_documento", schema="exemplo")
+@Table(name="t_documento", schema="documento")
 
 public class Documento {
 	
 	@Id
 	@Column(name="cd_documento")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	
 	@Column(name="nm_orgao", nullable = false)
 	private String nomeOrgao;
 
-	@Id
-	@Column(name="cd_assunto_basico", nullable=false)
-	private String assuntoBasico;
+	@OneToMany(mappedBy = "cd_especie")
+	private Long especieDocumento;
 
-	@Column(name="cd_tipo_documento", nullable = false)
-	private String tipoDocumento;
-	
-	@Column(name="nr_tipo_documento", nullable = false)
-	private Integer numeroTipoDocumento;
-	
+	@OneToMany(mappedBy = "cd_assunto_basico")
+	private Long assuntoBasico;
 
-	@Override
-	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return super.equals(obj);
-	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
-	
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
-	}
+	@Column(name="nm_titulo_documento", nullable = false)
+	private String nomeTituloDocumento;
+
+	@Column(name = "nr_secundario_assunto")
+	private String numeroSecundarioAssunto;
 
 }
